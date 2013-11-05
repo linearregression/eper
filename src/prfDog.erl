daaba40cb6ac4ddf3c1cc4e96aaee02710c6b311
mats cronqvist <masse@klarna.com>
2013-11-05 18:38:47 +0100

    fix prfDog bugs en masse

--- src/prfDog.erl
+++ src/prfDog.erl
@@ -45,30 +45,23 @@ rec_info(ld) -> record_info(fields,ld);
 rec_info(_)  -> [].
 
 init(Args) ->
-  LD = #ld{args=Args,acceptor=accept(producer,56669,sock_opts())},
-  watchdog:add_send_subscriber(udp,"localhost",56669,LD#ld.cookie),
+  LD = #ld{args=Args,acceptor=accept(56669,sock_opts())},
+  watchdog:add_send_subscriber(tcp,"localhost",56669,LD#ld.cookie),
   LD.
 
 handle_call(get_data,_,LD) ->
   {LD#ld.msg,LD#ld{msg=orddict:new()}}.
 
-%% M=fun({Proto,_,IP,PortNo,Payload})->{Proto,IP,PortNo,length(Payload)}end.
-%% F=fun(G)->receive X -> erlang:display(M(X)), G(G) end end.
-%% L=fun()->{ok,Sck}=gen_udp:open(16#BEBE,[{active,true}]),F(F)end.
-%% register(udp_echo,spawn(L)).
-
-handle_info({new_socket,producer,Sock},LD) ->
+handle_info({new_socket,Sock},LD) ->
   %% we accepted a socket towards a producer.
-%%  inet:setopts(Sock,[{active,once}]),
   LD#ld{socket=[Sock|LD#ld.socket]};
 handle_info({tcp,Sock,Bin},LD) ->
   case lists:member(Sock,LD#ld.socket) of
     true ->
       %% got data from a known socket. this is good
-      gen_tcp:close(Sock),
-      {watchdog,Node,Trig,Msg} = prf_crypto:decrypt(LD#ld.cookie,Bin),
-      LD#ld{socket=LD#ld.socket--[Sock],
-            msg=orddict:store({Node,Trig},Msg,LD#ld.msg)};
+      {watchdog,Node,TS,Trig,Msg} = prf_crypto:decrypt(LD#ld.cookie,Bin),
+      LD#ld{socket=LD#ld.socket,
+            msg=orddict:store({Node,TS,Trig},Msg,LD#ld.msg)};
     false->
       %% got data from unknown socket. wtf?
       ?log([{data_from,Sock},{sockets,LD#ld.socket},{bytes,byte_size(Bin)}]),
@@ -85,15 +78,15 @@ handle_info(Msg,LD) ->
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% accept is blocking, so it runs in its own process
-accept(What,Port,Opts) ->
-  erlang:spawn_link(fun() -> acceptor(What,Port,Opts) end).
+accept(Port,Opts) ->
+  erlang:spawn_link(fun() -> acceptor(Port,Opts) end).
 
-acceptor(What,Port,Opts) ->
+acceptor(Port,Opts) ->
   {ok,ListenSock} = gen_tcp:listen(Port,Opts),
-  acceptor_loop(What,ListenSock).
+  acceptor_loop(ListenSock).
 
-acceptor_loop(What,ListenSock) ->
+acceptor_loop(ListenSock) ->
   {ok,Socket} = gen_tcp:accept(ListenSock),
-  ?MODULE ! {new_socket,What,Socket},
+  ?MODULE ! {new_socket,Socket},
   gen_tcp:controlling_process(Socket,whereis(?MODULE)),
-  acceptor_loop(What,ListenSock).
+  acceptor_loop(ListenSock).
